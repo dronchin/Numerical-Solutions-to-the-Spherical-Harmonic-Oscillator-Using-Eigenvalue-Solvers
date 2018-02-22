e71a6b762899c2d653e3135118dbd0b7e1e24a03
Nicolas Dronchi <dronchin@msu.edu>
2018-02-22 00:09:38 -0500

    added function to take eigen values out

--- jacobiMethod.cpp
+++ jacobiMethod.cpp
@@ -31,7 +31,6 @@ double maxoffdiag(mat A, int n, int* k, int* l){
       // cout << i <<", " << j << ", " << A(i,j) << endl;
       if(fabs(A(i,j)) > max){
         max = fabs(A(i,j));
-        cout << *k << endl;
         *k = i;
         *l = j;
       }
@@ -40,6 +39,54 @@ double maxoffdiag(mat A, int n, int* k, int* l){
   return max;
 }
 
+int rotate(mat &A, mat R, int n, int k, int l){
+  double s, c;
+  if(A(k,l) != 0){
+    double t, tau;
+    tau = (A(l,l) - A(k,k))/(2.0*A(k,l));
+    if ( tau > 0 ) {
+      t = 1.0/(tau + sqrt(1.0 + tau*tau));
+    } else {
+      t = -1.0/( -tau + sqrt(1.0 + tau*tau));
+    }
+
+  c = 1.0/sqrt(1+t*t);
+  s = c*t;
+
+  } else{
+    s = 0;
+    c = 1;
+  }
+  double a_kk, a_ll, a_ik, a_il;
+  a_kk = A(k,k);
+  a_ll = A(l,l);
+  A(k,k) = a_kk*c*c + a_ll*s*s - 2.0*c*s*A(k,l);
+  A(l,l) = a_kk*s*s + a_ll*c*c + 2.0*c*s*A(k,l);
+  A(k,l) = 0;
+  A(l,k) = 0;
+  for(int i = 0; i < n; i++){
+    if(i != k && i!= l){
+      a_ik = A(i,k);
+      a_il = A(i,l);
+      A(i,k) = c*a_ik - s*a_il;
+      A(k,i) = A(i,k);
+      A(i,l) = c*a_il + s*a_ik;
+      A(l,i) = A(i,l);
+    }
+  }
+  return 0;
+}
+
+vec getEigenvalues(mat A, int n){
+  vec Eigenvalues = zeros<vec>(n);
+  for(int i=0; i<n; i++){
+    Eigenvalues[i] = A(i,i);
+  }
+  return Eigenvalues;
+}
+
+
+
 
 int main(int argc, char* argv[]){
   // char* name = argv[1];
@@ -47,9 +94,15 @@ int main(int argc, char* argv[]){
   double h = 1/ (double) n;
   double hh = h*h;
   double max;
+  double eps = 1e-8;
+  double maxiterations = 10000; //(double)n*(double)n*(double)n;
+
 
   mat A = zeros<mat>(n,n);
+  mat Acopy = zeros<mat>(n,n);
   A = makeTridiagmat(n, A);
+  Acopy = makeTridiagmat(n,Acopy);
+  mat R = eye<mat>(n,n);
 
   int k;
   int l;
@@ -57,7 +110,20 @@ int main(int argc, char* argv[]){
   l = 0;
 
   max = maxoffdiag(A,n,&k,&l);
+  int iterations = 0;
+  while(fabs(max) > eps && (double) iterations < maxiterations){
+    max = maxoffdiag(A,n,&k,&l);
+    cout << max << endl;
+    rotate(A,R,n,k,l);
+    iterations++;
+  }
+  A.print("A: ");
 
+  vec B;
+  B = getEigenvalues(A, n);
+  B.print();
 
-
+  vec eigval;
+  eig_sym(eigval, Acopy);
+  eigval.print("eigval: ");
 }
